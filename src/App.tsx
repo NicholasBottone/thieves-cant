37e428c769a664f5db0fffde71d70164233a6709
Nicholas Bottone <nick@bottone.io>
2024-04-25 20:54:34 -0400

    Add basic frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+import { translateToEnglish, translateToThievesCant } from "./utils/translate";
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("");
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noopener noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noopener noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Thieve&rsquo;s Cant Translator</h1>
+      <input
+        type="textarea"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button onClick={() => setInput(translateToEnglish(input))}>
+        Translate to English
+      </button>
+      <button onClick={() => setInput(translateToThievesCant(input))}>
+        Translate to Thieve&rsquo;s Cant
+      </button>
+      <button onClick={() => setInput("")}>Clear</button>
     </>
   );
 }
