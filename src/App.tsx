ee3de3c6ccdb246dce7d1bc60376a67d5759cdac
Andrew Li <Andrewli2024@gmail.com>
2024-04-26 02:23:31 -0400

    Add clear input button

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { ChangeEvent, useState } from "react";
 import "./App.css";
 
 import { FaArrowRightArrowLeft } from "react-icons/fa6";
+import { MdClear } from "react-icons/md";
 
 import DragonBody from "./assets/dragon-body.png";
 import DragonHead from "./assets/dragon-head.png";
@@ -18,7 +19,7 @@ function App() {
   const [translatingFromLanguage, setTranslatingFromLanguage] = useState<
     "thieves' cant" | "english"
   >("english");
-  const [inputText, setInputText] = useState("Enter text here...");
+  const [inputText, setInputText] = useState("");
   const [output, setOutput] = useState<Result>();
 
   const translatingToLanguage =
@@ -29,7 +30,6 @@ function App() {
    */
   const handleInputTextChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     setInputText(e.target.value);
-
     if (translatingFromLanguage === "english") {
       setOutput(translateToThievesCant(inputText));
     } else {
@@ -50,6 +50,14 @@ function App() {
     }
   };
 
+  /**
+   * Handler for clearing the input text.
+   */
+  const handleClearTextClick = () => {
+    setInputText("");
+    setOutput(undefined);
+  };
+
   return (
     <>
       <div className="header">
@@ -57,11 +65,9 @@ function App() {
       </div>
 
       <div className="translation-container">
-        <div className="dragon-images-container">
-          <img id="dragon-head" src={DragonHead} />
-          <img id="dragon-body" src={DragonBody} />
-          <img id="dragon-tail" src={DragonTail} />
-        </div>
+        <img id="dragon-head" src={DragonHead} />
+        <img id="dragon-body" src={DragonBody} />
+        <img id="dragon-tail" src={DragonTail} />
         <div className="translation-header-container">
           <div className="left-header">{translatingFromLanguage}</div>
           <div className="middle-header">
@@ -72,10 +78,18 @@ function App() {
         <div className="translation-box-container">
           <div className="translation-box" id="box-from">
             <textarea
+              placeholder="Enter text here..."
               value={inputText}
               onChange={handleInputTextChange}
               className="input-text-area"
             />
+            {inputText && (
+              <MdClear
+                className="clear-icon"
+                size={24}
+                onClick={handleClearTextClick}
+              />
+            )}
           </div>
           <div className="translation-box" id="box-to">
             <p className="output-text-area">{output?.translation}</p>
